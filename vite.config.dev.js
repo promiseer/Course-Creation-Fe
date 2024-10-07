da497bc1cdeddfd428429dea013bc77349ae2f3b
ItelSqa <itelsqa@gmail.com>
2024-10-07 14:44:31 +0600

    Add dev config

---
+++ vite.config.dev.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: '../platform/',
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/wp-json': {
+        target: 'http://stephare-react.local',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
