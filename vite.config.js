b5d160802ed29ab34f66c9af96e30289d7223e49
ItelSqa <itelsqa@gmail.com>
2024-10-13 00:33:57 +0600

    Download from staging site

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,31 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './',
+//   server: {
+//     proxy: {
+//       "/wp-json": {
+//         target: "https://wordpress-1273862-4898429.cloudwaysapps.com",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: '../platform/',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
-      "/wp-json": {
-        target: "https://wordpress-1273862-4898429.cloudwaysapps.com",
+      '/wp-json': {
+        target: 'http://stephare-react.local',
         changeOrigin: true,
         secure: false,
       },
